62bb783abc2c002a97819ca1a548dd61ef591425
Weizhen Wang <wangwzhen@gmail.com>
2019-05-29 11:53:36 -0500

    add post request and add client.js

--- pg_intro/server/public/client.js
+++ pg_intro/server/public/client.js
@@ -2,4 +2,19 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('in jq');
+    displaySongs();
+}
+
+function displaySongs() {
+    $.ajax({
+        method: 'GET',
+        url: '/songs'
+    }).then(
+        (response) => {
+            console.log(response[0]);
+            response.forEach((song) => {
+                $('#ulSongs').append(`<li>${song.artist} - ${song.track} - ${song.published} - ${song.rank}</li>`);
+            } )
+        }
+    )
 }
--- pg_intro/server/server.js
+++ pg_intro/server/server.js
@@ -1,47 +1,64 @@
+//require express and create an instance of the express app
 const express = require('express');
 const app = express();
+//require bodyParser
 const bodyParser = require('body-parser');
 const PORT = 5000;
-//need to 'npm install pg' from the command line first
-//in order not to break the nodemon => command +tab to open a new command line window
+//need to 'npm install pg' from the command line first - in order not to break the nodemon => command +tab to open a new command line window
 //this will add a new dependency to package.json
-//pg for config to work => to connect to the database
+//require pg for config to work => to connect to the database
 const pg = require('pg');
-const Pool = pg.Pool; //create a pool of connections to our database (Pool is a class of pg)
-
+//create a pool of connections to our database (Pool is a class of pg)
+const Pool = pg.Pool; 
+//user bodyParser
 app.use(bodyParser.urlencoded({extended: true}));
+//use static files
 app.use(express.static('server/public'));
 
-//connect to the database
+//provide the info to connect to the database
 const config = {
     database: 'music_library',
     host: 'localhost',
-    port: 5432,// the port postgres running on (not the postico)
+    port: 5432,// the port postgres running on (not the postico - only the GUI)
 }
 
 const pool = new Pool(config); // create a new pool to our database
 //const pool = new require('pg).Pool(config)
 
-
+//get request: send a query to get all songs from the songs table in database and send back the array
 app.get('/songs', (req, res) => {
     //make a select query from the database 
     //SELECT * FROM "songs"
     pool.query('SELECT * FROM "songs";')
         .then((result) => { //response from the query above
-            //result => a massive obj where the rows is one property inside, additional properties like Fields, rowCount ...
-            console.log(result);
-            res.send(result.rows)
+            //result is a massive obj where the rows is one property inside, additional properties like Fields, rowCount ...
+            res.send(result.rows) //send only the rows property of the result object
         }). catch((error) => {
+            //catch the error if the above code doesn't work
             console.log('error with SELECT songs query', error); 
             res.sendStatus(500); //400 client mistake 500 server error => you don't want to send the error back to client side due to security reason
         })
-    //.then after the query is complete, res.send
 })
 
 app.post('/songs', (req, res) => {
     //INSERT INTO "songs" (columns) VALUES
-    res.sendStatus(201);
-})
+    pool.query(
+        //insert to the columns, use $1, $2...  to refer to the columns
+        `INSERT INTO "songs" ("rank", "track", "artist", "published") 
+        VALUES($1, $2, $3, $4);`, 
+        [req.body.rank, req.body.track, req.body.artist, req.body.published]) //the actual location where the value will be added - pg sanitize the data
+
+    .then(() => {
+        //send back success status
+        res.sendStatus(201);
+    }).catch((error) => {
+        //catch the error if there's any
+        console.log('error with insert', error);
+        //and send back status
+        res.sendStatus(500);
+    }
+    )
+});
 
 
 app.listen(PORT, () => {
